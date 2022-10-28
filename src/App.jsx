5e91f9b37dbab23ea9f781efb6b046ef052ac024
cofeek-codes <kormyshev11@mail.ru>
2022-10-28 23:15:12 +0500

    audio recorder

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import useRecorder from './hooks/useRecorder'
+
+function App() {
+	let [audioURL, isRecording, startRecording, stopRecording] = useRecorder()
+	return (
+		<div className='App'>
+			<audio src={audioURL} controls />
+			<button onClick={startRecording} disabled={isRecording}>
+				start recording
+			</button>
+			<button onClick={stopRecording} disabled={!isRecording}>
+				stop recording
+			</button>
+		</div>
+	)
+}
+
+export default App
